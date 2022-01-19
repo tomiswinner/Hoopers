9e7a119a00b9452feb311e61edf8a2a70f0a40fc
tomiswinner <mouseofsack911@gmail.com>
2022-01-19 04:45:19 +0000

    [add] add CourtFav and EventFav

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_043450) do
+ActiveRecord::Schema.define(version: 2022_01_19_044348) do
 
   create_table "areas", force: :cascade do |t|
     t.integer "prefecture_id", null: false
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2022_01_19_043450) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["prefecture_id"], name: "index_areas_on_prefecture_id"
+  end
+
+  create_table "court_favorites", force: :cascade do |t|
+    t.integer "court_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["court_id"], name: "index_court_favorites_on_court_id"
+    t.index ["user_id"], name: "index_court_favorites_on_user_id"
   end
 
   create_table "courts", force: :cascade do |t|
@@ -39,6 +48,15 @@ ActiveRecord::Schema.define(version: 2022_01_19_043450) do
     t.datetime "updated_at", null: false
     t.index ["area_id"], name: "index_courts_on_area_id"
     t.index ["user_id"], name: "index_courts_on_user_id"
+  end
+
+  create_table "event_favorites", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_favorites_on_event_id"
+    t.index ["user_id"], name: "index_event_favorites_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
