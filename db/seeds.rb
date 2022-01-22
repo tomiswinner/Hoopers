6300d2c055db3a4bfa4cf3864ffa9cf98fba5639
tomiswinner <mouseofsack911@gmail.com>
2022-01-22 13:01:02 +0000

    [add] add resas func to register prefs and areas in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+Resas_endpoint = 'https://opendata.resas-portal.go.jp'
+Pref_path = '/api/v1/prefectures'
+City_path = '/api/v1/cities'
+
+
+def get_json(url)
+  uri = URI.parse(URI.escape(url))
+
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+
+
+  param = {}
+  param['X-API-KEY'] = ENV['RESAS_KEY']
+
+  req = Net::HTTP::Get.new(uri.request_uri, param)
+  res = https.request(req)
+
+  json = res.body
+  result = JSON.parse(json)
+
+  return result
+end
+
+
+def register_prefecutres
+  data = get_json(Resas_endpoint + Pref_path)
+
+  data['result'].each do |val|
+    Prefecture.create!(
+      name: val['prefName']
+      )
+  end
+end
+
+def register_areas
+  data = get_json(Resas_endpoint + City_path)
+
+  data['result'].each do |val|
+    puts val['prefCode']
+    puts val['cityName']
+  end
+end
+
+# register_areas
+# register_prefecutres
