1f710e639a07a00b0c6bcffaad26a43ee24a9810
tomiswinner <mouseofsack911@gmail.com>
2022-01-23 08:06:13 +0000

    [fix] add name col to user migration & lat,lng to court migration

--- db/migrate/20220118174808_devise_create_users.rb
+++ db/migrate/20220118174808_devise_create_users.rb
@@ -7,6 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
 
+      t.string :name,               null: false
       t.boolean :is_active,         null: false, default: true
 
       ## Recoverable
