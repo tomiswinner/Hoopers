5cf0c9d38babeddd760320bf99829aa1d9a43467
tomiswinner <mouseofsack911@gmail.com>
2022-01-24 02:54:40 +0000

    [add] add select area & prefs function to courts index.html

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -1,7 +1,22 @@
 class CourtsController < ApplicationController
   def index
+    @prefectures = Prefecture.all
     if params[:keyword]
       @courts = Court.where('name LIKE ?', "%#{:keyword}%")
+    end
+
+    if params[:prefecture_id]
+      @areas = Area.where(prefecture_id: params[:prefecture_id])
+    end
+
+    if params[:area_ids]
+      logger.debug 'だ'
+      logger.debug params[:area_ids]
+    end
+
+    respond_to do |f|
+      f.html
+      f.js
     end
   end
 
