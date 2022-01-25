2d76008eb6ca0cee086fbcaaaab960cc085bfbcb
tomiswinner <mouseofsack911@gmail.com>
2022-01-25 13:59:00 +0000

    [save] just save this state to github

--- spec/system/courts_spec.rb
+++ spec/system/courts_spec.rb
@@ -4,12 +4,19 @@ RSpec.describe 'Court', type: :system do
   describe "GET /index" do
     describe '地区検索機能' do
       before do
-        FactoryBot.create_list(:court, 3)
+        @factory_courts = FactoryBot.create_list(:court, 3)
       end
-      context 'area_id = [ 1, 2 ] データ[ 1, 2, 3 ]なら' do
+      context 'area_id = [ 1, 2 ] データ[ 1, 2, 3 ]なら', js: true do
         it 'area_id = 1 or 2 のコートのみが返ってくる' do
           visit courts_path
           click_on '絞り込み'
+          select(@factory_courts[0].fetch_pref_name, from:'prefs_select')
+          sleep 5
+          puts page.html
+          # check(Area.find(@factory_courts[0].area_id).name)
+          # check(Area.find(@factory_courts[1].area_id).name)
+          click_on('絞り込む')
+
           expect(@courts).to eq(Court.where(area_id: 1).or(Court.where(area_id: 2)))
         end
       end
