832f7759abfd70cd3d4f941711fad8b533af599d
tomiswinner <mouseofsack911@gmail.com>
2022-01-26 17:46:29 +0900

    [add] add court time search conditino

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -3,17 +3,24 @@ class CourtsController < ApplicationController
     @prefectures = Prefecture.all
     @courts = Court.none
     if params[:keyword]
-      @courts = Court.where('name LIKE ?', "%#{:keyword}%")
+      @courts = @courts.or(Court.where('name LIKE ?', "%#{:keyword}%"))
     end
 
-    if params[:prefecture_id]
-      @areas = Area.where(prefecture_id: params[:prefecture_id])
+    if params.dig(:prefecture, :id)
+      @areas = Area.where(prefecture_id: params.dig(:prefecture, :id))
     end
 
     if params.dig(:Area, :area_ids)
       params.dig(:Area, :area_ids).each do |area_id|
         @courts = @courts.or(Court.where(area_id: area_id.to_i))
       end
+    end
+    
+    # 時間が一つでも入力されていれば
+    if params[:Court].values.any? {|elem| !( elem.empty?)}
+      # open_time,close time はそれぞれ 分まで一緒に入れるようvalidation 入れる
+      @courts = @courts.or(Court.where())
+      
     end
 
     respond_to do |f|
--- spec/system/courts_spec.rb
+++ spec/system/courts_spec.rb
@@ -1,34 +1,50 @@
 require 'rails_helper'
-require 'database_cleaner/active_record'
+
 
 RSpec.describe 'Court', type: :system do
+  after(:suite) do
+  end
   describe "GET /index" do
     describe '地区検索機能' do
       before(:all) do
-        DatabaseCleaner.strategy = :transaction
-        DatabaseCleaner.start
         @factory_courts = FactoryBot.create_list(:court, 3)
       end
       after(:all) do
-        DatabaseCleaner.clean
       end
       context 'area_id = [ 1, 2 ] 存在するデータのidが[ 1, 2, 3 ]なら' do
         it 'area_id = 1 or 2 のコートが返ってくる' do
           visit courts_path(Area: {area_ids: [ @factory_courts[0].area_id, @factory_courts[1].area_id ]})
-          expect(page).to have_text(@factory_courts[0].name)&&have_text(@factory_courts[1].name)
+          expect(page).to have_content(@factory_courts[0].name)&&have_content(@factory_courts[1].name)
         end
         it 'area_id = 3 のコートは返ってこない' do
           visit courts_path(Area: {area_ids: [ @factory_courts[0].area_id, @factory_courts[1].area_id ]})
-          expect(page).not_to have_text(@factory_courts[2].name)
+          expect(page).not_to have_content(@factory_courts[2].name)
         end
       end
       context 'area_id = [] 存在するデータ[ 1, 2, 3 ]なら' do
-        it '一致するコートがありません' do
+        it '一致するコートがありませんと表示される' do
           visit courts_path
-          byebug
-          expect(page).to have_text('一致するコートがありません')
+          puts page.html
+          expect(page).to have_content('一致するコートがありません')
         end
       end
     end
+    describe '時間検索機能' do
+      before(:all) do
+        ['11:00','12:00','13:00'].each do |open|
+          ['22:00','23:00','24:00'].each do |close|
+            FactoryBot.create(:court, open_time: opne, close_time: close)
+          end
+        end
+      end
+
+      context '入力time 12:00~23:00 存在するデータ 11~22,12~22,13~22,11~23,12~23,13~23,11~24,12~24,13~24' do
+        it '12~22,13~22,12~23,13~23のデータが返ってくる' do
+          visit courts_path
+          expect(page).to have_content('address')
+        end
+      end
+    end
+    
   end
 end
