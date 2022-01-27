932a2119f60ab306b22922d2376ada306d5cf297
tomiswinner <mouseofsack911@gmail.com>
2022-01-28 01:59:59 +0900

    [add] add function to convert_time_to_past_sec

--- app/models/court.rb
+++ app/models/court.rb
@@ -11,4 +11,10 @@ class Court < ApplicationRecord
   def fetch_pref_name
     return Prefecture.find(Area.find(area_id).prefecture_id).name
   end
+
+  def convert_time_to_past_sec(hours, mins)
+    hours_sec = hours.to_i * 60 * 60
+    mins_sec = mins.to_i * 60
+    return hours_sec + mins_sec
+  end
 end
