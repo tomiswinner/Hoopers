766409b2433cd93aa2070e06cefca94bc101b69b
tomiswinner <mouseofsack911@gmail.com>
2022-01-30 02:10:13 +0900

    [add] add court_review new create

--- app/controllers/court_reviews_controller.rb
+++ app/controllers/court_reviews_controller.rb
@@ -3,7 +3,16 @@ class CourtReviewsController < ApplicationController
     @court_review = CourtReview.new()
   end
 
-  def create; end
+  def create
+    @court_review = CourtReview.new(court_review_params)
+    if @court_review.save
+      flash.now[:notice] = 'レビューが投稿されました！'
+      redirect_to court_path(@court_review.court_id)
+    else
+      flash.now[:alert] = 'レビュー投稿に失敗しました'
+      render :new
+    end
+  end
 
   def index
     @court = Court.find(params[:court_id])
@@ -13,4 +22,9 @@ class CourtReviewsController < ApplicationController
   def edit; end
 
   def update; end
+
+  private
+    def court_review_params
+      params.require(:court_review).permit(:accessibility, :security, :quality, :court_id, :user_id)
+    end
 end
