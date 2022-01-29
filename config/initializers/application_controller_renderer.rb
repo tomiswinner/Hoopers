b01d4d80d445e04a95e0326d09c26f1daed4f8d3
tomiswinner <mouseofsack911@gmail.com>
2022-01-29 14:07:11 +0900

    [add] add refile & show.html outline

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1bd6c82a501f0b5082ea50f7384eb0c0250a87a6365503d73764c151b476b8f1e39233b13dd331e2ed8ec88af198fb4558615750d06e8ba86e32df7c8e7501ea'
