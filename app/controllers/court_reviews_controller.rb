af17622a15b5b8d870cadea64c4f179df03d27ee
tomiswinner <mouseofsack911@gmail.com>
2022-01-30 13:21:20 +0900

    [add] add edit confirm user page & controller

--- app/controllers/court_reviews_controller.rb
+++ app/controllers/court_reviews_controller.rb
@@ -10,7 +10,36 @@ class CourtReviewsController < ApplicationController
     @court_reviews = @court.court_reviews
   end
 
-  def edit; end
+  def edit
+    @user = current_user
+  end
 
-  def update; end
+  def confirm
+    @user = current_user
+  end
+
+  def update
+    @customer = current_customer
+
+    if params[:is_deactivation?]
+      @user.update(is_active: false)
+      flash[:notice] = "退会処理が完了しました。"
+      sign_out_and_redirect(current_user)
+
+    else
+      if @customer.update(customer_params)
+        flash[:notice] = "更新が完了しました"
+        redirect_to user_mypage_path
+
+      else
+        flash[:alert] = "更新に失敗しました"
+        render :edit
+      end
+    end
+  end
+
+  private
+    def user_params
+      param.require(:user).permit(:name, :email)
+    end
 end
