d23381a73616dac16603be230737aca12509801f
tomiswinner <mouseofsack911@gmail.com>
2022-02-01 13:29:58 +0900

    [add] add switching review edit / new

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :courts,           dependent: :destroy
-  has_many :event_reviews,    dependent: :destroy
+  has_many :court_reviews,    dependent: :destroy
   has_many :court_histories,  dependent: :destroy
   has_many :court_favorites,  dependent: :destroy
   has_many :events,           dependent: :destroy
@@ -26,6 +26,14 @@ class User < ApplicationRecord
 
   def favorites_this_event?(event_id)
     if  event_favorites.find_by(event_id: event_id)
+      return true
+    else
+      return false
+    end
+  end
+
+  def reviewed_this_court?(court_id)
+    if  court_reviews.find_by(court_id: court_id)
       return true
     else
       return false
