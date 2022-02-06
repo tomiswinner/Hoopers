e77ddd38579a7a38a96e30edb61186aade349763
tomiswinner <mouseofsack911@gmail.com>
2022-02-06 13:10:29 +0900

    [add] add destroy & [fix] fix flash msg in event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,32 +6,27 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(events_params)
     if @event.save
-      flash.now[:notice] = 'イベントが投稿されました'
+      flash[:notice] = 'イベントが投稿されました'
       redirect_to(event_path(@event.id))
     else
       err_msg = "イベント投稿に失敗しました。\n"
       @event.errors.full_messages.each do |msg|
-        err_msg += msg + "\n"
+        err_msg += "#{msg}\n"
       end
       flash.now[:alert] = err_msg
       render :confirm
     end
-
   end
 
   def index
     @events = Event.all
 
-    if params.dig(:court_id)
-      @events = Event.where(court_id: params.dig(:court_id))
-    end
+    @events = Event.where(court_id: params[:court_id]) if params[:court_id]
 
     @courts = Court.where(confirmation_status: true).where(business_status: true)
 
     # リファクタリング、これ切り出せない？他のコントローラーに
-    if params.dig(:prefecture, :id)
-      @areas = Area.where(prefecture_id: params.dig(:prefecture, :id))
-    end
+    @areas = Area.where(prefecture_id: params.dig(:prefecture, :id)) if params.dig(:prefecture, :id)
 
     if params.dig(:Area, :area_ids)
       @area_search_res = Court.none
@@ -43,19 +38,18 @@ class EventsController < ApplicationController
     end
 
     @events = Event.where(court_id: @courts.pluck(:id))
-    puts @events
 
     # リファクタリング予知あり
     # validationは後ほど
 
-    if is_time_filled_in?('open') && is_time_filled_in?('close')
+    if time_filled_in?('open') && time_filled_in?('close')
       open_time = extract_formatted_time_from_params('open')
       close_time = extract_formatted_time_from_params('close')
       @events = @events.where('open_time >= ?', open_time).where('close_time <= ?', close_time)
-    elsif is_time_filled_in?('open')
+    elsif time_filled_in?('open')
       open_time = extract_formatted_time_from_params('open')
       @events = @events.where('open_time >= ?', open_time)
-    elsif is_time_filled_in?('close')
+    elsif time_filled_in?('close')
       close_time = extract_formatted_time_from_params('close')
       @events = @events.where('close_time <= ?', close_time)
     end
@@ -70,64 +64,88 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
-  def edit; end
+  def edit
+    @event = Event.find(params[:id])
+  end
 
-  def update; end
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(events_params)
+      flash[:notice] = 'イベントが修正されました'
+      redirect_to(event_path(@event.id))
+    else
+      err_msg = "イベントの修正に失敗しました。\n"
+      @event.errors.full_messages.each do |msg|
+        err_msg += "#{msg}\n"
+      end
+      flash.now[:alert] = err_msg
+      render :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      flash[:notice] = 'イベントを削除しました'
+      redirect_to(root_path)
+    else
+      flash.now[:alert] = 'イベントの削除に失敗しました'
+      render :edit
+    end
+  end
 
   def confirm
     @event = Event.new(events_params)
-    if @event.invalid?
-      err_msg = "イベント投稿に必要な内容が不足しています。\n"
-      @event.errors.full_messages.each do |msg|
-        err_msg += msg + "\n"
-      end
-      flash.now[:alert] = err_msg
-      render :new
+    return unless @event.invalid?
+
+    err_msg = "イベント投稿に必要な内容が不足しています。\n"
+    @event.errors.full_messages.each do |msg|
+      err_msg += "#{msg}\n"
     end
+    flash.now[:alert] = err_msg
+    render :new
   end
 
   def address; end
 
   def court_select
     res = fetch_geocoding_response(params.dig(:court, :address))
-    if !(res.nil?) && res.message == 'OK'
+    if !res.nil? && res.message == 'OK'
       geocoded_data = JSON.parse(res.body)
-      @center_lat = geocoded_data["results"][0]['geometry']['location']['lat']
-      @center_lng = geocoded_data["results"][0]['geometry']['location']['lng']
+      @center_lat = geocoded_data['results'][0]['geometry']['location']['lat']
+      @center_lng = geocoded_data['results'][0]['geometry']['location']['lng']
       @courts = Court.where('? <= latitude', @center_lat - Lat_range).where('? >= latitude', @center_lat + Lat_range)
-      @courts = @courts.where('? <= longitude', @center_lng - Lng_range).where('? >= longitude', @center_lng + Lng_range)
-
+      @courts = @courts.where('? <= longitude', @center_lng - Lng_range).where('? >= longitude',
+                                                                               @center_lng + Lng_range)
     else
       # リファクタリング
-      flash.now[:alert] = "エラーが発生しました。"
+      flash.now[:alert] = 'エラーが発生しました。'
+      render :address
     end
   end
 
   private
-    def events_params
-      return params.require(:event).permit(:user_id, :court_id, :name, :image, :description, :condition, :contact, :open_time, :close_time, :status)
-    end
 
-    def extract_formatted_time_from_params(str)
-      datetime = Time.new(
-        yaer = params.dig(:court, :"#{str}_time(1i)"),
-        mon =  params.dig(:court, :"#{str}_time(2i)"),
-        day =  params.dig(:court, :"#{str}_time(3i)"),
-        hour = params.dig(:court, :"#{str}_time(4i)"),
-        min =  params.dig(:court, :"#{str}_time(5i)"),
-        )
-      return datetime
-    end
+  def events_params
+    return params.require(:event).permit(:user_id, :court_id, :name, :image, :description, :condition, :contact,
+                                         :open_time, :close_time, :status)
+  end
 
-    def is_time_filled_in?(str)
-      [*1..5].each do|n|
-        if params.dig(:court, :"#{str}_time(#{n}i)").blank?
-          return false
-        end
-      end
-      return true
-    end
+  def extract_formatted_time_from_params(str)
+    datetime = Time.zone.local(
+      yaer: params.dig(:court, :"#{str}_time(1i)"),
+      mon: params.dig(:court, :"#{str}_time(2i)"),
+      day: params.dig(:court, :"#{str}_time(3i)"),
+      hour: params.dig(:court, :"#{str}_time(4i)"),
+      min: params.dig(:court, :"#{str}_time(5i)")
+    )
+    return datetime
+  end
 
+  def time_filled_in?(str)
+    [*1..5].each do |n|
+      return false if params.dig(:court, :"#{str}_time(#{n}i)").blank?
+    end
+    return true
+  end
 end
